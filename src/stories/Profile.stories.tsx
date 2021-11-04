8179f5ac65e0f7278ba20fa911e5a1f8b57173a8
Matheus Santos <matheus.psantos2016@gmail.com>
2021-11-04 19:09:41 -0300

    feat: add Profile component

---
+++ src/stories/Profile.stories.tsx
@@ -0,0 +1,15 @@
+import { Story, Meta } from "@storybook/react";
+import { Profile, ProfileProps } from "../components/Profile";
+
+export default {
+  title: "Data/Profile",
+  component: Profile,
+} as Meta;
+
+const Template: Story<ProfileProps> = (args) => <Profile {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  name: "Daniel Bonfacio",
+  description: "editor ha muito tempo",
+};
